d733b99e50a96c1d031531b58ef1362d9275c686
Manyo <agusmangini@hotmail.com>
2022-10-27 22:33:05 -0300

    Implemento dotenv en connection

--- api/connection/connection.js
+++ api/connection/connection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
+require('dotenv').require();
 
 const mysqlConnecction = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Manyito123!',
-    database: 'samuidb',
-    port: '3306'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 })
 
 mysqlConnecction.connect(err => {
