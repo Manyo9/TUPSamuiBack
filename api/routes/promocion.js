352e80960b19b20372e54d97853bdee9e6d9af4e
Abraguas <fmoises2002@gmail.com>
2022-10-31 19:45:27 -0300

    Agrego post de promocion

--- api/routes/promocion.js
+++ api/routes/promocion.js
@@ -33,6 +33,63 @@ router.get('/detalles/:id', [authJwt.verifyToken, authJwt.invalidTokenCheck], (r
             }
         })
 });
+router.post('/', [authJwt.verifyToken, authJwt.invalidTokenCheck, authJwt.isEmployee], async (req, res) => {
+
+    const { nombre, descripcion, precioPuntos, detalles, fechaDesde, fechaHasta } = req.body;
+    await mysqlConnecction.query('SET TRANSACTION ISOLATION LEVEL READ COMMITTED');
+    await mysqlConnecction.beginTransaction();
+    mysqlConnecction.query('call spRegistrarPromocion(?,?,?,?,?,@id); select @id as id;', [nombre, descripcion, precioPuntos, fechaDesde, fechaHasta],
+        async (err, rows, fields) => {
+            if (!err) {
+                const idPromocion = rows[1][0].id;
+
+                try {
+                    for (const detalle of detalles) {
+                        const { producto, cantidad} = detalle;
+                        mysqlConnecction.query('call spRegistrarDetallePromocion(?,?,?);', [idPromocion, producto.id, cantidad],
+                            async (err, rows, fields) => {
+                                if (err) {
+                                    console.error(err);
+                                    console.log("rollback");
+                                    mysqlConnecction.rollback();
+                                    res.status(500).json({
+                                        "ok": false,
+                                        "mensaje": "Error al registrar promocion"
+
+                                    });
+                                    return;
+                                }
+                            });
+                    }
+                    res.status(201).json({
+                        "ok": true,
+                        "mensaje": "Promocion creada con éxito"
+                    });
+                    await mysqlConnecction.commit();
+                }
+                catch (e) {
+                    console.error(e);
+                    console.log("rollback");
+                    res.status(500).json({
+                        "ok": false,
+                        "mensaje": "Error al registrar promocion"
+
+                    });
+                    await mysqlConnecction.rollback();
+                }
+
+            } else {
+                console.log(err);
+                console.log("rollback");
+                mysqlConnecction.rollback();
+                res.status(500).json({
+                    "ok": false,
+                    "mensaje": "Error al registrar promocion"
+
+                });
+            }
+        });
+});
 
 
 module.exports = router
