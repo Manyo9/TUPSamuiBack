d6960be477f6ab8e16557b8b80f60c72f0540a4a
Abraguas <fmoises2002@gmail.com>
2022-11-01 20:50:37 -0300

    Agrego get/:id y put de socios

--- api/routes/socio.js
+++ api/routes/socio.js
@@ -9,21 +9,21 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 
-router.get('/', 
+router.get('/',
     [authJwt.verifyToken,
     authJwt.invalidTokenCheck,
     authJwt.isEmployee], (req, res) => {
-    mysqlConnecction.query('call spObtenerSocios();',
-        (err, rows, fields) => {
-            if (!err) {
-                res.status(200).json({ "ok": true, "resultado": rows[0]});
-                console.log(rows[0]);
-            } else {
-                res.status(500).json({ "ok": false, "mensaje": "Error al listar socios" })
-                console.log(err);
-            }
-        })
-});
+        mysqlConnecction.query('call spObtenerSocios();',
+            (err, rows, fields) => {
+                if (!err) {
+                    res.status(200).json({ "ok": true, "resultado": rows[0] });
+                    console.log(rows[0]);
+                } else {
+                    res.status(500).json({ "ok": false, "mensaje": "Error al listar socios" })
+                    console.log(err);
+                }
+            })
+    });
 
 router.post('/', (req, res) => {
     const { idUsuario, nombre, apellido, domicilio, email, dni, telefono } = req.body;
@@ -45,35 +45,85 @@ router.post('/', (req, res) => {
 
 });
 
-router.delete('/:id', 
+router.delete('/:id',
     [authJwt.verifyToken,
-     authJwt.invalidTokenCheck,
-     authJwt.checkIdSocio],
-    (req, res) => {    
-    mysqlConnecction.query('call spDarDeBajaSocio(?,@status); select @status as status;', [req.params['id']],
-        (err, rows, fields) => {
-            if (!err) {
-                const status = rows[1][0].status;
-                if (status == 1) {
-                    res.status(200).json({
-                        "ok": true,
-                        "mensaje": "Socio dado de baja con éxito"
-                    });
-                } else if (status == 0){
-                    res.status(404).json({
+    authJwt.invalidTokenCheck,
+    authJwt.checkIdSocio],
+    (req, res) => {
+        mysqlConnecction.query('call spDarDeBajaSocio(?,@status); select @status as status;', [req.params['id']],
+            (err, rows, fields) => {
+                if (!err) {
+                    const status = rows[1][0].status;
+                    if (status == 1) {
+                        res.status(200).json({
+                            "ok": true,
+                            "mensaje": "Socio dado de baja con éxito"
+                        });
+                    } else if (status == 0) {
+                        res.status(404).json({
+                            "ok": false,
+                            "mensaje": `No se encontró al socio con id ${req.params['id']}`
+                        });
+                    }
+
+                } else {
+                    console.log(err);
+                    res.status(500).json({
                         "ok": false,
-                        "mensaje": `No se encontró al socio con id ${req.params['id']}`
+                        "mensaje": "Error al dar de baja socio"
                     });
                 }
+            })
+    });
 
+router.get('/:id',
+    [authJwt.verifyToken,
+    authJwt.invalidTokenCheck],
+    (req, res) => {
+        if (req.data.rol === 'Admin' || req.data.rol === 'Empleado' || req.params['id'] == req.data.idSocio) {
+            mysqlConnecction.query('call spObtenerSocioById(?);', req.params['id'],
+                (err, rows, fields) => {
+                    if (!err) {
+                        res.status(200).json({ "ok": true, "resultado": rows[0] });
+                        console.log(rows[0]);
+                    } else {
+                        res.status(500).json({ "ok": false, "mensaje": "Error al listar socios" })
+                        console.log(err);
+                    }
+                })
+        } else {
+            res.status(403).json({ "ok": false, "mensaje": "Usted no tiene los permisos requeridos para acceder a este recurso." });
+        }
+    });
+
+//para 
+router.put('/',
+    [
+        authJwt.verifyToken,
+        authJwt.invalidTokenCheck
+    ],    
+    (req, res) => {
+        //Si se quiere que admin y empleado puedan modificar DNI hay que crear otra ruta con otro sp
+        if (req.data.rol === 'Admin' || req.data.rol === 'Empleado' || req.params['id'] == req.data.idSocio) {
+        const { id, nombre, apellido, domicilio, email, telefono } = req.body;
+        mysqlConnecction.query('call spModificarSocio(?,?,?,?,?,?)', [ nombre, apellido, domicilio, email, telefono, id],
+        (err, rows, fields) => {
+            if (!err) {
+                res.status(200).json({
+                    "ok": true,
+                    "mensaje": "Socio actualizado con éxito"
+                });
             } else {
                 console.log(err);
                 res.status(500).json({
                     "ok": false,
-                    "mensaje": "Error al dar de baja socio"
+                    "mensaje": "Error al actualizar socio"
                 });
             }
-        })
-});
+        });
+        } else {
+            res.status(403).json({ "ok": false, "mensaje": "Usted no tiene los permisos requeridos para acceder a este recurso." });
+        }
+    });
 
 module.exports = router
