66bd1469d8fe75e3bc089660b0a28fdcf42492fd
Abraguas <fmoises2002@gmail.com>
2022-11-01 15:58:43 -0300

    Agrego get /productos/activos

--- api/routes/producto.js
+++ api/routes/producto.js
@@ -14,103 +14,115 @@ router.get('/',
     authJwt.invalidTokenCheck,
     authJwt.isEmployee]
 
-    ,(req, res) => {
-    mysqlConnecction.query('call spObtenerProductos();',
-        (err, rows, fields) => {
-            if (!err) {
-                res.status(200).json({ "ok": true, "resultado": rows[0] });
-            } else {
-                res.status(500).json({ "ok": false, "mensaje": "Error al listar productos" })
-                console.log(err);
-            }
-        })
-});
-
+    , (req, res) => {
+        mysqlConnecction.query('call spObtenerProductos();',
+            (err, rows, fields) => {
+                if (!err) {
+                    res.status(200).json({ "ok": true, "resultado": rows[0] });
+                } else {
+                    res.status(500).json({ "ok": false, "mensaje": "Error al listar productos" })
+                    console.log(err);
+                }
+            })
+    });
+// Todos los activos o no activos, para empleados
+router.get('/activos'
+    , (req, res) => {
+        mysqlConnecction.query('call spObtenerProductosActivos();',
+            (err, rows, fields) => {
+                if (!err) {
+                    res.status(200).json({ "ok": true, "resultado": rows[0] });
+                } else {
+                    res.status(500).json({ "ok": false, "mensaje": "Error al listar productos" })
+                    console.log(err);
+                }
+            })
+    });
 router.get('/:id',
     [authJwt.verifyToken,
     authJwt.invalidTokenCheck,
     authJwt.isEmployee],
     (req, res) => {
-    mysqlConnecction.query('call spObtenerProductoPorID(?)', [req.params['id']],
-        (err, rows, fields) => {
-            if (!err) {
-                if (rows.length > 0) {
-                    res.status(200).json({ "ok": true, "resultado": rows[0] });
+        mysqlConnecction.query('call spObtenerProductoPorID(?)', [req.params['id']],
+            (err, rows, fields) => {
+                if (!err) {
+                    if (rows.length > 0) {
+                        res.status(200).json({ "ok": true, "resultado": rows[0] });
+                    } else {
+                        res.status(404).json({ "ok": false, "resultado": [] });
+                    }
                 } else {
-                    res.status(404).json({ "ok": false, "resultado": [] });
+                    console.log(err);
                 }
-            } else {
-                console.log(err);
-            }
-        })
-});
+            })
+    });
 
-router.post('/', 
+router.post('/',
     [authJwt.verifyToken,
     authJwt.invalidTokenCheck,
     authJwt.isEmployee], (req, res) => {
-        
-    const { nombre, precio, descripcion, observaciones, activo, puntosGanados, urlImagen } = req.body;
-    mysqlConnecction.query('call spInsertarProducto(?,?,?,?,?,?,?)', [nombre, precio, descripcion, observaciones, activo, puntosGanados, urlImagen],
-        (err, rows, fields) => {
-            if (!err) {
-                res.status(201).json({
-                    "ok": true,
-                    "mensaje": "Producto creado con éxito"
-                });
-            } else {
-                console.log(err);
-                res.status(500).json({
-                    "ok": false,
-                    "mensaje": "Error al crear producto"
-                });
-            }
-        });
-});
 
-router.put('/', 
+        const { nombre, precio, descripcion, observaciones, activo, disponible, puntosGanados, urlImagen } = req.body;
+        mysqlConnecction.query('call spInsertarProducto(?,?,?,?,?,?,?,?)', [nombre, precio, descripcion, observaciones, activo, disponible, puntosGanados, urlImagen],
+            (err, rows, fields) => {
+                if (!err) {
+                    res.status(201).json({
+                        "ok": true,
+                        "mensaje": "Producto creado con éxito"
+                    });
+                } else {
+                    console.log(err);
+                    res.status(500).json({
+                        "ok": false,
+                        "mensaje": "Error al crear producto"
+                    });
+                }
+            });
+    });
+
+router.put('/',
     [authJwt.verifyToken,
     authJwt.invalidTokenCheck,
     authJwt.isEmployee],
     (req, res) => {
-    const { id, nombre, precio, descripcion, observaciones, activo, puntosGanados, urlImagen } = req.body;
-    mysqlConnecction.query('call spActualizarProducto(?,?,?,?,?,?,?,?)', [id, nombre, precio, descripcion, observaciones, activo, puntosGanados, urlImagen],
-        (err, rows, fields) => {
-            if (!err) {
-                res.status(201).json({
-                    "ok": true,
-                    "mensaje": "Producto actualizado con éxito"
-                });
-            } else {
-                console.log(err);
-                res.status(500).json({
-                    "ok": false,
-                    "mensaje": "Error al actualizar producto"
-                });
-            }
-        });
-});
+        const { id, nombre, precio, descripcion, observaciones, activo, puntosGanados, urlImagen } = req.body;
+        mysqlConnecction.query('call spActualizarProducto(?,?,?,?,?,?,?,?)', [id, nombre, precio, descripcion, observaciones, activo, puntosGanados, urlImagen],
+            (err, rows, fields) => {
+                if (!err) {
+                    res.status(201).json({
+                        "ok": true,
+                        "mensaje": "Producto actualizado con éxito"
+                    });
+                } else {
+                    console.log(err);
+                    res.status(500).json({
+                        "ok": false,
+                        "mensaje": "Error al actualizar producto"
+                    });
+                }
+            });
+    });
 
 router.delete('/:id',
     [authJwt.verifyToken,
     authJwt.invalidTokenCheck,
     authJwt.isEmployee], (req, res) => {
 
-    mysqlConnecction.query('call spBorrarProducto(?)', [req.params['id']],
-        (err, rows, fields) => {
-            if (!err) {
-                res.status(200).json({
-                    "ok": true,
-                    "mensaje": "Producto eliminado con éxito"
-                });
-            } else {
-                console.log(err);
-                res.status(500).json({
-                    "ok": false,
-                    "mensaje": "Error al eliminar producto"
-                });
-            }
-        });
-});
+        mysqlConnecction.query('call spBorrarProducto(?)', [req.params['id']],
+            (err, rows, fields) => {
+                if (!err) {
+                    res.status(200).json({
+                        "ok": true,
+                        "mensaje": "Producto eliminado con éxito"
+                    });
+                } else {
+                    console.log(err);
+                    res.status(500).json({
+                        "ok": false,
+                        "mensaje": "Error al eliminar producto"
+                    });
+                }
+            });
+    });
 
 module.exports = router;
